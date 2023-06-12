0632fc303e2e12105c62d58e9f4623033b8e0b4d
Insori <s2110@e-mirim.hs.kr>
2023-06-12 13:31:34 +0900

    Shadowing 예제

--- ShadowingHidingPrac/Program.cs
+++ ShadowingHidingPrac/Program.cs
@@ -8,8 +8,11 @@ namespace ShadowingHidingPrac
 {
     internal class Program
     {
+        public static int number = 10;
         static void Main(string[] args)
         {
+            int number = 20;
+            Console.WriteLine(number);  //20이 출력됨
         }
     }
 }
